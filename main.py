e5a13872a634a2649e148e261a688e939bb38bb1
Késsia Silva <kessiasilvadealmeida@gmail.com>
2025-10-04 15:23:14 -0300

    dicionário de usuários feito

--- main.py
+++ main.py
@@ -1,17 +1,37 @@
-from flask import Flask, render_template #importando a classe Flask do pacote Flask
+from flask import Flask, render_template, request, redirect, url_for #importando a classe Flask do pacote Flask
 
 app = Flask(__name__)  # cria a aplicação
+
+#dicionario de clientes
+usuarios = {
+    "felipe@gmail.com": {"senha": "12345", "nome": "Felipe Cerqueira"},
+    "Larissa@gmail.com": {"senha": "12345", "nome": "Larissa"},
+    "Joao@gmail.com": {"senha": "12345", "nome": "Joao"}
+}
 
 @app.route("/")        # define uma rota
 def home():
     return render_template("home.html")
 
-@app.route("/login")
+@app.route("/login", methods = ["GET", "POST"])
 def login():
+    mensagem = None
+    email = ""
+    senha = ""
+
+    if request.method == "POST": # só processa se o form for enviado 
+        email = request.form.get("email")
+        senha = request.form.get("senha")
+
+    # Verifica se o e-mail existe e se a senha está correta
+    if email in usuarios and usuarios[email]["senha"] == senha:
+         # Redireciona para home e envia o nome do usuário
+         nome = usuarios[email]["nome"]
+         return redirect(url_for("home", usuario=nome))
+
     return render_template("login.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
-
 
 #python main.py <-- para rodar
