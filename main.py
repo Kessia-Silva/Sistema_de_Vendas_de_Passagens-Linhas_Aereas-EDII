bfb3eeaa49d44cb4471b441667ed9b6014850db0
Késsia Silva <kessiasilvadealmeida@gmail.com>
2025-10-07 15:56:05 -0300

    criação da pasta arquivos, dicionário de usuários e remodelação de login

--- main.py
+++ main.py
@@ -1,13 +1,6 @@
 from flask import Flask, render_template, request, redirect, url_for #importando a classe Flask do pacote Flask
-
+from arquivos.usuarios import usuarios # chamando o arquivo de usuarios
 app = Flask(__name__)  # cria a aplicação
-
-#dicionario de clientes
-usuarios = {
-    "felipe@gmail.com": {"senha": "12345", "nome": "Felipe Cerqueira"},
-    "Larissa@gmail.com": {"senha": "12345", "nome": "Larissa"},
-    "Joao@gmail.com": {"senha": "12345", "nome": "Joao"}
-}
 
 @app.route("/")        # define uma rota
 def home():
@@ -15,21 +8,20 @@ def home():
 
 @app.route("/login", methods = ["GET", "POST"])
 def login():
-    mensagem = None
-    email = ""
-    senha = ""
 
-    if request.method == "POST": # só processa se o form for enviado 
+    mensagem = "" # só processa se o form for enviado 
+    if request.method == "POST":
         email = request.form.get("email")
         senha = request.form.get("senha")
 
-    # Verifica se o e-mail existe e se a senha está correta
-    if email in usuarios and usuarios[email]["senha"] == senha:
-         # Redireciona para home e envia o nome do usuário
-         nome = usuarios[email]["nome"]
-         return redirect(url_for("home", usuario=nome))
+        # Verifica email e senha
+        for usuario in usuarios:
+            if usuario["email"] == email and usuario["senha"] == senha:
+                return redirect(url_for("home"))
+            else:
+                mensagem = "Email ou senha incorretos!"
 
-    return render_template("login.html")
+    return render_template("login.html", mensagem=mensagem)
 
 if __name__ == "__main__":
     app.run(debug=True)
