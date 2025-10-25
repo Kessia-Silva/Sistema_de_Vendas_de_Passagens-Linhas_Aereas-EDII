88617df9e841e70b7bf6ec8446084a4e085249ef
Ana Paula <166036823+AnaPaulaSPin@users.noreply.github.com>
2025-10-25 16:29:45 -0300

    Adicionando a função Adm

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request, redirect, url_for #importando a classe Flask do pacote Flask
 from arquivos.usuarios import usuarios # chamando o arquivo de usuarios
-from arquivos.voos import voos # chamando o arquivo de usuarios
+from arquivos.voos import voos # chamando o arquivo de voos
+from arquivos.adm import adms
 
 app = Flask(__name__)  # cria a aplicação
 
@@ -13,8 +14,12 @@ def home():
 def reserva():
     return render_template("reservar_voo.html", voos_agendados = voos)
 
-@app.route("/login", methods = ["GET", "POST"])
-def login():
+@app.route("/editar_voos")   
+def editar():
+    return render_template("editar_voo.html", voos_agendados = voos)
+
+@app.route("/login_user", methods = ["GET", "POST"])
+def login_user():
     mensagem = "" # só processa se o form for enviado 
     if request.method == "POST":
         email = request.form.get("email")
@@ -27,7 +32,24 @@ def login():
             else:
                 mensagem = "Email ou senha incorretos!"
 
-    return render_template("login.html", mensagem=mensagem)
+    return render_template("login_user.html", mensagem=mensagem)
+
+
+@app.route("/login_adm", methods = ["GET", "POST"])
+def login_adm():
+    mensagem = "" # só processa se o form for enviado 
+    if request.method == "POST":
+        email = request.form.get("email")
+        senha = request.form.get("senha")
+
+        # Verifica email e senha
+        for adm in adms:
+            if adm["email"] == email and adm["senha"] == senha:
+                return redirect(url_for("editar"))
+            else:
+                mensagem = "Email ou senha incorretos!"
+
+    return render_template("login_adm.html", mensagem=mensagem)
 
 
 # Rodar a aplicação
