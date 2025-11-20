624c7b09ac432d90bc2b1050f7cf895d41b9ca31
Késsia Silva <kessiasilvadealmeida@gmail.com>
2025-11-20 01:37:29 -0300

    colocando mais respostas para o chat e adicionando um delay na resposta

--- arquivos/respostas.py
+++ arquivos/respostas.py
@@ -3,6 +3,12 @@ respostas = {
     ("tchau", "adeus", "até mais", "ate mais", "até logo", "ate logo"): "Até mais! Boa viagem!",
     ("qual é o preço do voo?", "preço voo"): "Os preços variam conforme a data e a companhia aérea.",
     ("como faço para comprar uma passagem?", "comprar passagem", "como comprar passagens?"): "Você pode comprar passagem ao logar na sua conta, na tela inicial",
-
-
+    ("quais destinos vocês oferecem?", "destinos", "para onde posso viajar"): "Oferecemos voos para Brasil, Coreia, Japão, França, Alemanha, Estados Unidos e muito mais",
+    ("quais são os horários dos voos?", "horário dos voos", "quando partem os voos"): "Os horários dos voos estão disponíveis na página de consultar voos.",
+    ("como cancelar uma passagem?", "cancelar voo", "como faço para cancelar"): "Para cancelar uma passagem, acesse sua conta, vá em 'Minhas Reservas' e selecione a opção de cancelamento.",
+    ("tem desconto para estudantes?", "desconto estudante", "preço estudante"): "Não! ainda nao temos, porém temos vários descontos que rolam ao decorrer do ano, fique atento.",
+    ("como acumulo milhas?", "milhas", "acúmulo de milhas"): "Você acumula milhas a cada voo realizado; vo pode visualizar suas milhas em saldo de milhas",
+    ("qual é a política de reembolso?", "reembolso", "cancelamento com reembolso"): "O reembolso depende do tipo de passagem. Consulte a política ao comprar seu bilhete.",
+    ("como faço login?", "entrar", "login", "acessar conta"): "Clique no botão de login no topo da página e insira seu e-mail e senha.",
+    ("esqueci minha senha", "recuperar senha", "senha perdida"): "Você pode recuperar sua senha clicando em 'Esqueci minha senha' na tela de login."
 }
